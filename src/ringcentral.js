78ac2e88e07f3f8a1ab72571ddb045dd896fd33b
Tyler Long <tyler4long@gmail.com>
2018-07-18 13:26:47 +0800

    Initial code

---
+++ src/ringcentral.js
@@ -0,0 +1,68 @@
+import RingCentralRest from 'ringcentral-js-concise'
+import WS from 'ws'
+import uuid from 'uuid/v1'
+
+class RingCentral extends RingCentralRest {
+  constructor (clientId, clientSecret, httpsServer, wssServer) {
+    super(clientId, clientSecret, httpsServer)
+    this.ws = new WS(wssServer)
+    for (const event of ['close', 'error', 'message', 'open', 'ping', 'pong', 'unexpected-response', 'upgrade']) {
+      this.ws.on(event, (...args) => this.emit(event, ...args))
+    }
+  }
+
+  request (config) {
+    return new Promise((resolve, reject) => {
+      const uid = uuid()
+      this.ws.send(JSON.stringify(
+        [
+          {
+            'type': 'ClientRequest',
+            'messageId': uid,
+            'method': config.method,
+            'path': config.url,
+            'headers': this._patchHeaders(config.headers)
+          }
+        ]
+      ))
+      const handler = data => {
+        const [meta, body] = JSON.parse(data)
+        if (meta.messageId === uid && meta.type === 'ClientRequest') {
+          this.ws.off('message', handler)
+          if (meta.status > 199 && meta.status < 300) {
+            resolve({
+              data: body,
+              status: meta.status,
+              headers: meta.headers,
+              config
+            })
+          } else {
+            reject(new WSGError({
+              data: body,
+              status: meta.status,
+              headers: meta.headers,
+              config
+            }))
+          }
+        }
+      }
+      this.ws.on('message', handler)
+    })
+  }
+}
+
+class WSGError extends Error {
+  constructor (response) {
+    super()
+    this.response = response
+  }
+}
+
+RingCentral.SANDBOX_HTTPS_SERVER = RingCentral.SANDBOX_SERVER
+RingCentral.PRODUCTION_HTTPS_SERVER = RingCentral.PRODUCTION_SERVER
+delete RingCentral.SANDBOX_SERVER
+delete RingCentral.PRODUCTION_SERVER
+RingCentral.SANDBOX_WSS_SERVER = 'wss://ws-api.devtest.ringcentral.com/ws'
+RingCentral.PRODUCTION_WSS_SERVER = 'wss://ws-api.ringcentral.com/ws'
+
+export default RingCentral
---
+++ test/ringcentral.spec.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv'
+
+import RingCentral from '../src/ringcentral'
+
+dotenv.config()
+
+const rc = new RingCentral(
+  process.env.RINGCENTRAL_CLIENT_ID,
+  process.env.RINGCENTRAL_CLIENT_SECRET,
+  process.env.RINGCENTRAL_SERVER_URL,
+  process.env.RINGCENTRAL_WSG_URL
+)
+
+;(async () => {
+  await rc.authorize({
+    username: process.env.RINGCENTRAL_USERNAME,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD
+  })
+
+  const r = await rc.get('/restapi/v1.0/account/~/extension/~')
+  console.log(r.data)
+})()
