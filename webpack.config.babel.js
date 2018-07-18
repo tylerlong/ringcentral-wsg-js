c9dd4c4f2b0b929194fb20e195c33e36242355a6
Tyler Long <tyler4long@gmail.com>
2018-07-18 10:24:04 +0800

    Setup project structure

---
+++ webpack.config.babel.js
@@ -0,0 +1,21 @@
+import path from 'path'
+
+export default {
+  mode: 'production',
+  entry: './src/ringcentral.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'ringcentral.js',
+    library: 'RingCentral',
+    libraryTarget: 'umd',
+    globalObject: 'this' // fix window undefined issue in node
+  },
+  externals: {
+    axios: {
+      commonjs: 'axios',
+      commonjs2: 'axios',
+      amd: 'axios',
+      root: 'axios'
+    }
+  }
+}
